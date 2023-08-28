41e8c094868c3b5e89994f01946fb42bbcd96a68
nilasperlenwelt <125917768+nilasperlenwelt@users.noreply.github.com>
2023-08-28 15:11:06 +0200

    Delete NilasPerlenwelt directory
    Delete old content

--- NilasPerlenwelt/Nilas_Perlenwelt_files/Nilas_Perlenwelt.js
+++
@@ -1,9 +0,0 @@
-// Created by iWeb 3.0.4 local-build-20230220
-
-setTransparentGifURL('Media/transparent.gif');function applyEffects()
-{var registry=IWCreateEffectRegistry();registry.registerEffects({shadow_1:new IWShadow({blurRadius:4,offset:new IWPoint(1.4142,1.4142),color:'#000000',opacity:0.500000}),shadow_2:new IWShadow({blurRadius:4,offset:new IWPoint(1.4142,1.4142),color:'#000000',opacity:0.500000}),shadow_0:new IWShadow({blurRadius:4,offset:new IWPoint(1.4142,1.4142),color:'#000000',opacity:0.500000})});registry.applyEffects();}
-function hostedOnDM()
-{return false;}
-function onPageLoad()
-{loadMozillaCSS('Nilas_Perlenwelt_files/Nilas_PerlenweltMoz.css')
-adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');fixAllIEPNGs('Media/transparent.gif');IMpreload('Nilas_Perlenwelt_files','shapeimage_2','0');IMpreload('Nilas_Perlenwelt_files','shapeimage_2','1');IMpreload('Nilas_Perlenwelt_files','shapeimage_3','0');IMpreload('Nilas_Perlenwelt_files','shapeimage_3','1');IMpreload('Nilas_Perlenwelt_files','shapeimage_3','2');IMpreload('Nilas_Perlenwelt_files','shapeimage_4','0');IMpreload('Nilas_Perlenwelt_files','shapeimage_4','1');IMpreload('Nilas_Perlenwelt_files','shapeimage_4','2');applyEffects()}
